d3e1cf59d0a4822c8426fb5bae8ca3d841b9fa36
Justin Wood <woody@apple.com>
2017-04-19 00:00:02 -0700

    Adding filtered topic, and energycurb

--- mqtt_helpers.js
+++ mqtt_helpers.js
@@ -3,11 +3,19 @@ mqtt = require('mqtt')
 
 publish_map = {}
 
+function fix_name(str) {
+    str = str.replace(/[+\\\&\*\%\$\#\@\!]/g, '')
+    str = str.replace(/\s/g, '_').trim().toLowerCase()
+    str = str.replace(/__/g, '_')
+    return str
+}
+
 exports.publish = function(client, topic, message) {
     if (client === null || topic === null) {
         logging.warn("empty client or topic passed into mqtt_helpers.publish")
         return
     }
+    topic = fix_name(topic)
 
     logging.log(" " + topic + ":" + message)
     if (publish_map[topic] !== message) {
