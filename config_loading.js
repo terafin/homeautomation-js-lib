2798c68ba0a117dc683d893939d3cbbb4efb8f0c
Justin Wood <woody@apple.com>
2017-04-26 10:19:10 -0700

    Better device config loading

--- config_loading.js
+++ config_loading.js
@@ -1,12 +1,12 @@
-fs = require('fs')
-path = require('path');
-watch = require('watch')
-yaml = require('js-yaml');
+const fs = require('fs')
+const path = require('path')
+const watch = require('watch')
+const yaml = require('js-yaml')
+const logging = require('./logging.js')
 
-configs = []
-config_path = null
+var configs = []
+var config_path = null
 
-logging = require('./logging.js')
 
 exports.load_path = function(in_path) {
     config_path = in_path
@@ -21,43 +21,113 @@ exports.get_configs = function() {
     return configs
 }
 
+exports.nameForTopic = function(in_topic) {
+    var foundName = null
+
+    configs.forEach(function(config_item) {
+        if (foundName !== null) return
+
+        Object.keys(config_item).forEach(function(key) {
+            if (foundName !== null) return
+
+            const map = config_item[key]
+            const topic = map['topic']
+
+            if (topic === in_topic)
+                foundName = map['name']
+
+        }, this)
+    }, this)
+
+    return foundName
+}
+
+exports.isVoiceEnabledForTopic = function(in_topic) {
+    var foundVoice = false
+    var voiceResult = false
+
+    configs.forEach(function(config_item) {
+        if (foundVoice === true) return
+
+        Object.keys(config_item).forEach(function(key) {
+            if (foundVoice === true) return
+
+            const map = config_item[key]
+
+            const topic = map['topic']
+            if (topic === in_topic) {
+                voiceResult = map['voice_control']
+                foundVoice = true
+            }
+
+        }, this)
+    }, this)
+
+    return voiceResult
+}
 
 exports.translate_to_topic = function(in_topic) {
-    found_topic = null
+    var found_topic = null
 
-    config.get_configs().forEach(function(config_item) {
-        all_configs = Object.keys(config_item)
+    configs.forEach(function(config_item) {
+        if (found_topic !== null) return
 
-        all_configs.forEach(function(key) {
-            map = config_item[key]
-            foundItem = map[in_topic]
-            if (found_topic === null && foundItem !== null && foundItem !== undefined) {
-                logging.log("  " + in_topic + " => " + foundItem)
-                found_topic = foundItem
-            }
-        }, this);
-    }, this);
+        Object.keys(config_item).forEach(function(key) {
+            if (found_topic !== null) return
+
+            const map = config_item[key]
+            const src_topic = map['change_topic']
+
+            if (src_topic === in_topic)
+                found_topic = map['topic']
+
+        }, this)
+    }, this)
 
     return found_topic
 }
 
 exports.translate_from_topic = function(in_topic) {
-    found_topic = null
+    var found_topic = null
 
-    config.get_configs().forEach(function(config_item) {
-        all_configs = Object.keys(config_item).forEach(function(key) {
-            map = config_item[key]
+    configs.forEach(function(config_item) {
+        if (found_topic !== null) return
 
-            all_topics = Object.keys(map).forEach(function(this_topic) {
-                if (found_topic === null && in_topic == map[this_topic]) {
-                    logging.log("  " + in_topic + " => " + this_topic)
-                    found_topic = this_topic
-                }
-            }, this);
-        }, this);
-    }, this);
+        Object.keys(config_item).forEach(function(key) {
+            if (found_topic !== null) return
+
+            const map = config_item[key]
+
+            const topic = map['topic']
+            const src_topic = map['change_topic']
+            if (topic === in_topic)
+                found_topic = src_topic
+
+        }, this)
+    }, this)
 
     return found_topic
+}
+
+function print_device_config() {
+    configs.forEach(function(config_item) {
+        Object.keys(config_item).forEach(function(key) {
+            logging.log(' Device [' + key + ']')
+            const map = config_item[key]
+
+            const topic = map['topic']
+            const src_topic = map['change_topic']
+            const voice = map['voice_control']
+            const name = map['name']
+
+            logging.log('            name: ' + name)
+            logging.log('           topic: ' + topic)
+            logging.log('       src_topic: ' + src_topic)
+            logging.log('           voice: ' + voice)
+            logging.log('')
+
+        }, this)
+    }, this)
 }
 
 function load_device_config() {
@@ -66,19 +136,20 @@ function load_device_config() {
 
         logging.log('Loading configs at path: ' + config_path)
         if (err) {
-            throw err;
+            throw err
         }
 
         files.map(function(file) {
-            return path.join(config_path, file);
+            return path.join(config_path, file)
         }).filter(function(file) {
-            return fs.statSync(file).isFile();
+            return fs.statSync(file).isFile()
         }).forEach(function(file) {
-            logging.log(" - Loading: " + file);
-            doc = yaml.safeLoad(fs.readFileSync(file, 'utf8'));
+            logging.log(' - Loading: ' + file)
+            const doc = yaml.safeLoad(fs.readFileSync(file, 'utf8'))
             configs.push(doc)
-        });
+        })
 
         logging.log('...done loading configs')
+        print_device_config()
     })
 }
