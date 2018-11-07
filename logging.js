70549fb7b6b27e8d21cc7dc0fbe632c39e09fab3
terafin <jwood@me.com>
2018-11-07 08:45:54 -0800

    Updating for better logging

--- logging.js
+++ logging.js
@@ -13,18 +13,32 @@ if (_.isNil(logName)) {
 
 const winston = require('winston')
 
+require('winston-daily-rotate-file')
+
+const dailyFileLogger = new (winston.transports.DailyRotateFile)({
+	level: 'silly',
+	filename: 'application-%DATE%.log',
+	datePattern: 'YYYY-MM-DD-HH',
+	zippedArchive: true,
+	maxSize: '20m',
+	maxFiles: '7d',
+	dirname: '/var/log'
+})
+
+const consoleLogger = new winston.transports.Console( {level: 'info',
+	format: winston.format.combine(
+		winston.format.label({label: '[' + logName + ']'}),
+		winston.format.colorize(), 
+		winston.format.timestamp({
+			format: 'YYYY-MM-DD HH:mm:ss'
+		}), 
+		winston.format.printf(info => `${info.timestamp} ${info.label} ${info.level}: ${info.message}`)
+	),
+})
+
 var logger = winston.createLogger({
-	levels: winston.config.syslog.levels,
-	transports: [new winston.transports.Console( {level: 'info',
-		format: winston.format.combine(
-			winston.format.label({label: '[' + logName + ']'}),
-			winston.format.colorize(), 
-			winston.format.timestamp({
-				format: 'YYYY-MM-DD HH:mm:ss'
-			}), 
-			winston.format.printf(info => `${info.timestamp} ${info.label} ${info.level}: ${info.message}`)
-		),
-	})]
+	levels: winston.config.cli.levels,
+	transports: [consoleLogger, dailyFileLogger]
 })
   
 console.log('starting logging for: ' + logName)
