a430a7895ba1c9525d393c41c94c0b73461f765d
Justin Wood <jwood@me.com>
2019-06-08 20:52:53 -0700

    Fixing logging

--- logging.js
+++ logging.js
@@ -1,4 +1,5 @@
 const _ = require('lodash')
+const fs = require('fs')
 const disableSyslog = process.env.DISABLE_SYSLOG
 
 var logName = process.env.name
@@ -15,46 +16,59 @@ const winston = require('winston')
 
 require('winston-daily-rotate-file')
 
-const logFormat = function(shouldColor) {
-	if ( shouldColor) { 
+const logFormat = function (shouldColor) {
+	if (shouldColor) {
 		return winston.format.combine(
-			winston.format.label({label: '[' + logName + ']'}),
-			winston.format.colorize(), 
+			winston.format.label({ label: '[' + logName + ']' }),
+			winston.format.colorize(),
 			winston.format.timestamp({
 				format: 'YYYY-MM-DD HH:mm:ss'
-			}), 
-			winston.format.printf(info => `${info.timestamp} ${info.label} ${info.level}: ${info.message}`)) 
+			}),
+			winston.format.printf(info => `${info.timestamp} ${info.label} ${info.level}: ${info.message}`))
 	}
 
 	return winston.format.combine(
-		winston.format.label({label: '[' + logName + ']'}),
+		winston.format.label({ label: '[' + logName + ']' }),
 		winston.format.timestamp({
 			format: 'YYYY-MM-DD HH:mm:ss'
-		}), 
-		winston.format.printf(info => `${info.timestamp} ${info.label} ${info.level}: ${info.message}`)) 
+		}),
+		winston.format.printf(info => `${info.timestamp} ${info.label} ${info.level}: ${info.message}`))
 
 }
 
-const dailyFileLogger = new (winston.transports.DailyRotateFile)({
-	level: 'silly',
-	filename: 'application-%DATE%.log',
-	datePattern: 'YYYY-MM-DD-HH',
-	zippedArchive: true,
-	maxSize: '20m',
-	maxFiles: '7d',
-	dirname: '/var/log',
-	format: logFormat(false)
-})
+const canWrite = function(directory) {
+	var stat = fs.statSync(directory)
 
-const consoleLogger = new winston.transports.Console( {level: 'info',
+	// 2 is the octal value for chmod -w-
+	return !!(2 & (stat.mode & parseInt('777', 8)).toString(8)[0]) //first char is the owner
+}
+
+var dailyFileLogger = null
+const loggingPath = '/var/log'
+
+if (!canWrite(loggingPath)) {
+	dailyFileLogger = new (winston.transports.DailyRotateFile)({
+		level: 'silly',
+		filename: 'application-%DATE%.log',
+		datePattern: 'YYYY-MM-DD-HH',
+		zippedArchive: true,
+		maxSize: '20m',
+		maxFiles: '7d',
+		dirname: loggingPath,
+		format: logFormat(false)
+	})
+}
+
+const consoleLogger = new winston.transports.Console({
+	level: 'info',
 	format: logFormat(true)
 })
 
 var logger = winston.createLogger({
 	levels: winston.config.cli.levels,
-	transports: [consoleLogger, dailyFileLogger]
+	transports: _.isNil(dailyFileLogger) ? [consoleLogger] : [consoleLogger, dailyFileLogger]
 })
-  
+
 console.log('starting logging for: ' + logName)
 
 if (disableSyslog !== false) {
