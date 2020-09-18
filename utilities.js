ee7a190d3a7c51ac888b5660ac88d4fa07c7dba2
Justin Wood <jwood@me.com>
2020-09-18 09:39:26 -0700

    Adding options

--- utilities.js
+++ utilities.js
@@ -9,8 +9,33 @@ const MIN_VALUES_FOR_RUNNING_AVERAGE_THRESHOLD = 3
 const THRESHOLD_TO_THROW_AWAY = 6
 const MAX_VALUES_TO_THROW_AWAY = 2
 
-module.exports.add_running_average = function(key, value) {
+const default_options = {
+    values_for_running_average: VALUES_FOR_RUNNING_AVERAGE,
+    min_values_for_running_average_threshold: MIN_VALUES_FOR_RUNNING_AVERAGE_THRESHOLD,
+    threshold_to_throw_away: THRESHOLD_TO_THROW_AWAY,
+    max_values_to_throw_away: MAX_VALUES_TO_THROW_AWAY
+}
+module.exports.add_running_average = function(key, value, options) {
+    if (_.isNil(options)) {
+        options = default_options
+    }
+
+    var values_for_running_average = options['values_for_running_average']
+    var min_values_for_running_average_threshold = options['min_values_for_running_average_threshold']
+    var threshold_to_throw_away = options['threshold_to_throw_away']
+    var max_values_to_throw_away = options['max_values_to_throw_away']
+
+    if (_.isNil(values_for_running_average))
+        values_for_running_average = VALUES_FOR_RUNNING_AVERAGE
+    if (_.isNil(min_values_for_running_average_threshold))
+        min_values_for_running_average_threshold = MIN_VALUES_FOR_RUNNING_AVERAGE_THRESHOLD
+    if (_.isNil(threshold_to_throw_away))
+        threshold_to_throw_away = THRESHOLD_TO_THROW_AWAY
+    if (_.isNil(max_values_to_throw_away))
+        max_values_to_throw_away = MAX_VALUES_TO_THROW_AWAY
+
     var values = running_average_map[key]
+
     if (_.isNil(values)) {
         values = []
         values_thrown_away_counter[key] = 0
@@ -18,15 +43,15 @@ module.exports.add_running_average = function(key, value) {
 
     const current_average = module.exports.running_average(key)
     const already_thrown_away = values_thrown_away_counter[key]
-    const is_value_out_of_range = Math.abs(current_average - value) >= THRESHOLD_TO_THROW_AWAY
-    const do_we_have_enough_data = values.length >= MIN_VALUES_FOR_RUNNING_AVERAGE_THRESHOLD
+    const is_value_out_of_range = Math.abs(current_average - value) >= threshold_to_throw_away
+    const do_we_have_enough_data = values.length >= min_values_for_running_average_threshold
     const throw_away_value = (is_value_out_of_range && do_we_have_enough_data) ?
-        (already_thrown_away >= MAX_VALUES_TO_THROW_AWAY ? false : true) : false
+        (already_thrown_away >= max_values_to_throw_away ? false : true) : false
 
     if (!throw_away_value) {
         values.push(value)
 
-        if (values.length > VALUES_FOR_RUNNING_AVERAGE) {
+        if (values.length > values_for_running_average) {
             values.shift()
         }
         running_average_map[key] = values
